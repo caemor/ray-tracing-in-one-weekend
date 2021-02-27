c373a6ba3698b58292977c6aceb74bc934be4150
caemor <11088935+caemor@users.noreply.github.com>
2021-02-27 17:35:08 +0100

    Chapter 2 finished

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Raytracing in Rust
+
+Following the instructions from the "Ray Tracing in One Weekend - The Book Series" on https://raytracing.github.io/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+const IMAGE_WIDTH: usize = 256;
+const IMAGE_HEIGHT: usize = 256;
+
 fn main() {
-    println!("Hello, world!");
+    // Colors in Ascii
+    println!("P3");
+    // Columns and Rows
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    // 255 for Max Color
+    println!("255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.25;
+
+            let ir: u8 = (255.999 * r) as u8;
+            let ig: u8 = (255.999 * g) as u8;
+            let ib: u8 = (255.999 * b) as u8;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+    eprintln!("\nDone.");
 }
